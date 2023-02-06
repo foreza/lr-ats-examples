a49abc97651f91d6f61369ad37adb572ff2c68de
Jason C <jforeza@gmail.com>
2023-02-06 09:49:43 -0800

    Tweak POC to handle env better

---
+++ poc/envelope/iframes/inject-sample.js
@@ -0,0 +1,4 @@
+
+var frame = document.createElement("iframe");
+document.body.appendChild(frame);
+frame.src = "https://jasonthechiu.com/lr-ats-examples/poc/envelope/iframes/intermediate-frame.html";
