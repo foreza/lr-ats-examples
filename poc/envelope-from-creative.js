65265d48008002c0aef047a03e26071b039de919
jasonc-lr <93205858+jasonc-lr@users.noreply.github.com>
2024-09-30 15:29:50 -0700

    Update envelope-from-creative.js
    Add poller, add link append

--- poc/envelope-from-creative.js
+++ poc/envelope-from-creative.js
@@ -1,50 +1,65 @@
-function addEnvelopeViewToDom(){
+let retryCounter = 10; // for POC purposes, only
 
-            // Capture things with a span
-            var tSpan = document.createElement("span");
-            tSpan.id = "envelopeView";
-            tSpan.style = "position: absolute; display: block; top: 0; font-size: .1em; color: green; background: yellow;";
-            tSpan.textContent = "noEnvelope";
-            document.body.appendChild(tSpan);
+function addEnvelopeViewToDom() {
+
+   // Capture things with a span
+   var tSpan = document.createElement("span");
+   tSpan.id = "envelopeView";
+   tSpan.style = "position: absolute; display: block; top: 0; font-size: .1em; color: green; background: yellow;";
+   tSpan.textContent = "noEnvelope";
+   document.body.appendChild(tSpan);
+}
+
+// Set up a listener function to get envelope from the page (or in the future, app via some sort of JS/native bridge..)
+function receiveEnvelope(event) {
+   if (event && event.data && event.data.message === 'ats-modules-liveramp-envelope-result') {
+      console.log(event.data.result, event.origin);
+
+      // Clear the interval once we get it
+      clearInterval(envelopePoll);
+
+      try {
+         var tEnv = JSON.parse(event.data.result);
+         var tRef = document.getElementById("envelopeView");
+         tRef.textContent = tEnv.envelope;
+
+         // (POC) Append envelope dynamically to any / all links in the creative?
+         var tLinks = document.querySelectorAll("a");
+         for (t in tLinks) {
+            tLinks[t].href += "&lrEnv=" + tEnv.envelope;
+            console.log(tLinks[t]);
          }
 
-         // Adds this to the view
-         addEnvelopeViewToDom();
-         
-         // Set up a listener function to get envelope from the page (or in the future, app via some sort of JS/native bridge..)
-         function receiveEnvelope(event) {
-            if (event && event.data && event.data.message === 'ats-modules-liveramp-envelope-result') {
-               console.log(event.data.result, event.origin);
-               try {
-                  var tEnv = JSON.parse(event.data.result);
-                  var tRef = document.getElementById("envelopeView");
-                  tRef.textContent = tEnv.envelope;
+      } catch (e) {
+         // Handle error
+         console.error(e);
+      }
+   }
+}
 
-                  // We have the envelope - can we macro replace it in / do things with it?
 
-                  // var tLink1 = document.getElementById("clickUrlExample1");
-                  // var tLink2 = document.getElementById("clickUrlExample2");
+// Function to poll (just for POC purposes)
+function makeCallToParentForEnvelope() {
+   try {
+      top.postMessage('ats-modules-liveramp-envelope-request', '*');
+   } catch (e) {
+      if (typeof (top.contentWindow) != 'undefined') {
+         top.contentWindow.postMessage('ats-modules-liveramp-envelope-request', '*');
+      }
+   }
+}
 
-                  // tLink1.href += "&lrEnv=" + tEnv.envelope;
-                  // tLink2.href += "&lrEnv=" + tEnv.envelope;
 
-                  // console.log("clickUrlExample1: " + tLink1);
-                  // console.log("clickUrlExample2: " + tLink2);
 
-               } catch (e) {
-                  // Handle error
-                  console.error(e);
-               }
-            }
-         }
+// Subscribe to listener
+window.addEventListener('message', receiveEnvelope, false);
 
-         // Subscribe to listener
-         window.addEventListener('message', receiveEnvelope, false);
+// Adds this to the view
+addEnvelopeViewToDom();
 
-         try {
-            top.postMessage('ats-modules-liveramp-envelope-request', '*');
-         } catch (e) {
-            if (typeof (top.contentWindow) != 'undefined') {
-               top.contentWindow.postMessage('ats-modules-liveramp-envelope-request', '*');
-            }
-         }
+// Begin polling
+var envelopePoll = setInterval(() => {
+   retryCounter--;
+   console.log("Creative polling start: " + retryCounter + " more times");
+   makeCallToParentForEnvelope();
+}, 1000);
